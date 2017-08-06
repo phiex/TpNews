6ee6a2841039e0083fc67a5b6bd1ace311731143
phiex <13659756178@163.com>
2017-08-06 21:56:27 +0800

    2.增加了新闻主表

--- TpNews.sql
+++ TpNews.sql
@@ -13,3 +13,46 @@ CREATE TABLE `news_admin`(
    PRIMARY KEY (`admin_id`),
    KEY `username`(`username`)
 )ENGINE = MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+/*
+  菜单表
+ */
+CREATE TABLE `news_menu`(
+  `menu_id` SMALLINT(6) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '菜单id',
+  `name` VARCHAR(40) NOT NULL DEFAULT '' COMMENT '菜单名',
+  `parentid` SMALLINT(6) NOT NULL DEFAULT '0'COMMENT '父级id',
+  `m` VARCHAR(20) NOT NULL DEFAULT '',
+  `c` VARCHAR(20) NOT NULL DEFAULT '',
+  `f` VARCHAR(20) NOT NULL DEFAULT '',
+  `listorder` SMALLINT(6) UNSIGNED NOT NULL DEFAULT '0',
+  `status` TINYINT(1) NOT NULL DEFAULT '1',
+  `type` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`menu_id`),
+  KEY `listorder` (`listorder`),
+  KEY `parentid` (`parentid`),
+  KEY  `module` (`m`,`c`,`f`)
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET = utf8;
+
+/*
+  新闻主表
+ */
+create table `news_master`(
+  `news_id` mediumint(8) unsigned not null auto_increment comment '新闻主表id',
+  `catid` smallint(5) unsigned not null default '0',
+  `title` varchar(80) not null default '',
+  `small_title` varchar(80) not null default '',
+  `title_font_color` varchar(250) default null comment '标题颜色',
+  `thumb` varchar(100) not null default '',
+  `keyword` char(40) not null default '',
+  `description` char(40) not null comment '文章描述',
+  `listorder` tinyint(3) unsigned not null default '0',
+  `status` tinyint(1) not null default '1',
+  `copyfrom` varchar(250) default null comment'来源',
+  `username` char(20) not null,
+  `create_time` int(10) unsigned not null default '0',
+  `update_time` int(10) unsigned not null default '0',
+  `count` int(10) unsigned not null default '0',
+  PRIMARY KEY (`news_id`),
+  KEY `listorder`(`listorder`),
+  KEY `catid` (`catid`)
+)ENGINE=MyISAM auto_increment=1 default charset=utf8;
